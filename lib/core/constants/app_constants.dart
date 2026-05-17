d3fbaeffe7ba4fa49cbd687e73157f08bb1a5e7d
Yuri-cmd <yurim011218@gmail.com>
2026-05-16 20:15:08 -0500

    feat: add AppConstants class for centralizing API and configuration URLs

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,4 +1,5 @@
 class AppConstants {
   static const String baseUrl = 'https://magusemail.com/fitnessback/public/api';
+  static const String supportUrl = 'https://magusemail.com/fitnessback/public/support';
   static const String authTokenKey = 'auth_token';
 }
